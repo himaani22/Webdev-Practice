5a7b863b8902e74ef9e0200a2792759591e7dde6
Himani <Himani8809@gmail.com>
2025-07-27 19:41:33 +0530

    New commit

--- Javascript/Promise Methods/index.js
+++ Javascript/Promise Methods/index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // Promise.All
 
 function api1(){
@@ -62,65 +62,3 @@ let y= Promise.allSettled([api1(),api2(),api3()]).then(data =>{
 
 
 
-=======
-// Promise.All
-
-function api1(){
-    return new Promise ((res,rej) =>{
-        setTimeout(() => {
-            res("api1")
-        }, 4000);
-    });
-}
-
-function api2(){
-    return new Promise((res,rej) =>{
-        setTimeout( () =>{
-            res('api2')
-        },7000)
-    });
-}
-
-function api3 (){
-    return new Promise((res,rej) =>{
-        setTimeout(() => {
-            res("api3")
-        }, 2000);
-    });
-}
-
-let x= Promise.all([api1(),api2(),api3()]).then(data =>{
-    console.log(data);
-});
-
-
-// Promise.allsetted
-
-function api1(){
-    return new Promise ((res,rej) =>{
-        setTimeout(() => {
-            res("api1")                       
-        }, 4000);
-    });
-}
-
-function api2(){
-    return new Promise((res,rej) =>{
-        setTimeout( () =>{
-            res('api2')
-        },7000)
-    });
-}
-
-function api3 (){
-    return new Promise((res,rej) =>{
-        setTimeout(() => {
-            res("api3")
-        }, 2000);
-    });
-}
-
-let y= Promise.allSettled([api1(),api2(),api3()]).then(data =>{
-    console.log(data);
-});
->>>>>>> 4e94e0785104f1b631caf257dd4f80ba14e5ce0a
